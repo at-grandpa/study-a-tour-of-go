6d61a4e5072179c53cd32a8070cd9ecdca19bcaa
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-08-08 08:47:12 +0900

    until 02.basics/03.more-types/18.exercise-slices/exercise-slices.go

--- 02.basics/03.more-types/18.exercise-slices/exercise-slices.go
+++ 02.basics/03.more-types/18.exercise-slices/exercise-slices.go
@@ -3,6 +3,14 @@ package main
 import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
+	s := make([][]uint8, dy)
+	for yi := range s {
+		s[yi] = make([]uint8, dx)
+		for xi := range s[yi] {
+			s[yi][xi] = uint8((xi + yi) / 2)
+		}
+	}
+	return s
 }
 
 func main() {
