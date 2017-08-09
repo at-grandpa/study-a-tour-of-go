b5fcba8b2a425286011addffdb7296cd3e76a485
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-08-09 09:29:47 +0900

    until 03.methods-and-interfaces/01.methods-and-interfaces/03.methods-continued

---
+++ 03.methods-and-interfaces/01.methods-and-interfaces/03.methods-continued/methods-continued.go
@@ -0,0 +1,18 @@
+package main
+
+import "math"
+import "fmt"
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+func main() {
+	f := MyFloat(-math.Sqrt2) // MyFloat型にキャストしている
+	fmt.Println(f.Abs())
+}
