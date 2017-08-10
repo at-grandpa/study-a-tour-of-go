e38b40f9cc17213a567925b443e8d53137e88a49
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-08-10 09:46:27 +0900

    until 03.methods-and-interfaces/01.methods-and-interfaces/10.interfaces-are-implemented-implicitly

---
+++ 03.methods-and-interfaces/01.methods-and-interfaces/09.interfaces/interfaces.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+type Abser interface {
+	Abs() float64
+}
+
+func main() {
+	var a Abser
+	f := MyFloat(-math.Sqrt2)
+	v := Vertex{3, 4}
+
+	a = f  // a MyFloat implements Abser
+	a = &v // a *Vertex implements Abser
+
+	// In the following line, v is a Vertex (not * Vertex)
+	// and does NOT implement Abser.
+	// a = v  => cannot use v (type Vertex) as type Abser in assignment: Vertex does not implement Abser (Abs method has pointer receiver)
+
+	fmt.Println(a.Abs())
+}
