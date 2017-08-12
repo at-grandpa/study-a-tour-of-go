d2fb2d3b4d6c53c6aba6ac217ca9bb6607e3d96c
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-08-12 20:57:48 +0900

    until 04.concurrency/01.concurrency/05.select

---
+++ 04.concurrency/01.concurrency/03.buffered-channels/buffered-channels.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch := make(chan int, 2)
+	ch <- 1
+	ch <- 2
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	ch <- 3
+	ch <- 4
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	ch <- 5
+	ch <- 6
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+}
